1f57fd0efbe75b0d6848566439360c8c28b8506a
Yun Sung Choi <cyss0317@gmail.com>
2021-08-16 16:30:59 -0500

    Add Secret key

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,25 +1,56 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const keys = require('../../config/keys')
+
 
 router.post("/register", (req, res)=> {
-    User.findOne({email: req.body.email })
-      .then( user => {
-        if ( user ){
-          return res.status(400).json({email: "A user is already registered with this email"})
-        } else {
-          const newUser = new User({
-            email: req.body.email,
-            name: req.body.name,
-            businessOwner: req.body.businessOwner,
-            password: req.body.password,
-            phoneNumber: req.body.phoneNumber
+  User.findOne({email: req.body.email })
+    .then( user => {
+      if ( user ){
+        return res.status(400).json({email: "A user is already registered with this email"})
+      } else {
+        const newUser = new User({
+          email: req.body.email,
+          name: req.body.name,
+          businessOwner: req.body.businessOwner,
+          password: req.body.password,
+          phoneNumber: req.body.phoneNumber
+        })
+      
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then((user) => res.json(user))
+              .catch(err => console.log(err))
           })
-          newUser.save()
-            .then( user => res.send(user))
-            .catch(err => res.send(err));
+        })
+      }
+    })
+})
+
+router.post('/login', (req, res)=> {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user){
+        return res.status(404).json({ email: "This user does not exist" });
+      }
+
+      bcrypt.compare(password, user.password)
+      .then(isMatch => {
+        if(isMatch){
+        res.json({msg: "Success!"})
+        } else {
+          return res.status(400).json({password: "Incorrect password"});
         }
       })
+    })
 })
 
 router.get("/test", (req, res) => {

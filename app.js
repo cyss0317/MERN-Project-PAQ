2c9f99db7016dcadcc119d5c55626216d7664c1b
0skat <81402903+0skat@users.noreply.github.com>
2021-08-16 16:43:03 -0700

    frontend auth

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
+const Messages = require('./sms'); 
 
 mongoose
   .connect(db, { useNewUrlParser: true} )
@@ -15,3 +16,4 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000; 
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
+console.log(process.env);

9a1c605331474a22241dad088e6157b8f0c82e97
0skat <81402903+0skat@users.noreply.github.com>
2021-08-18 15:04:24 -0700

    changed gitignore due to merge conflct

---
+++ config/keys_dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mongoURI: "mongodb+srv://admin:51mEHWOGDWsCN2S6@mern.25ao6.mongodb.net/PAQdb",
+  secretOrKey: "q_k/KYa0qGvPz/s",
+  accountSid: 'AC267a3af176d41e44d27da3df82f94a13',
+  authToken: '50ca1189f0deadf35a4295db6a6f06e8',
+  phoneNumber: '+12546553803',
+  user: 'paqitup@gmail.com',
+  pass: 'kxpvgmastshomhcm'
+}

38f1a047fdba942e85c9b8e98db81984e29bfaa9
Yun Sung Choi <cyss0317@gmail.com>
2021-08-18 10:05:42 -0500

    Add shipment container

---
+++ frontend/src/components/shipments/shipment_container.js
@@ -0,0 +1,16 @@
+import {connect} from "react-redux";
+import ShipmentIndex from "./shipment_index";
+import { createNewShipment, fetchShipment, updateShipment } from "../../actions/shipment_actions";
+
+
+const mSTP = (state, ownProps) => ({
+    shipments: state.shipment
+})
+
+const mDTP = dispatch => ({
+    createNewShipment: shipment => dispatch(createNewShipment(shipment)),
+    fetchShipment: shipmentId => dispatch(fetchShipment(shipmentId)),
+    updateShipment: shipment => dispatch(updateShipment(shipment))
+})
+
+export default connect(mSTP, mDTP)(ShipmentIndex)
---
+++ frontend/src/components/shipments/shipment_index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class ShipmentIndex extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    componenetDidMount(){
+        this.props.fetchShipments
+    }
+
+    render(){
+        return(
+            
+        )
+    }
+}
+
+
+export default ShipmentIndex;

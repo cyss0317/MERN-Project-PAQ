f6ea23d44615f59a968716d3dccde173808615b7
Yun Sung Choi <cyss0317@gmail.com>
2021-08-18 17:38:09 -0500

    fixing heroku issue

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
-const path = require('path');
 const users = require("./routes/api/users");
 const User = require("./models/User");
 

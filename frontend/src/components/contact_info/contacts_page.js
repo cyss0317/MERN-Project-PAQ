7e20b4f12fc5e021fee15f0641e1fa43cff3e9b0
jangcla <jangcla@ymail.com>
2021-08-18 20:36:13 -0700

    css stuff

---
+++ frontend/src/components/contact_info/contacts_page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class ContactsPage extends React.Component {
+
+    render() {
+        return(
+            <div id='contact-container'>
+                Contacts for the Developers
+            </div>
+        )
+    }
+}
+
+export default ContactsPage;
